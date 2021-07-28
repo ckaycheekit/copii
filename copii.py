ecb166dfbe07417f2e1963d28aacfa66eca22de6
ckaycheekit <ckayyeah@gmail.com>
2021-07-29 00:52:01 +0800

    halfway thru landing page

--- copii.py
+++ copii.py
@@ -2,6 +2,52 @@ import tkinter as tk
 from database import Database
 
 
+# Global variables
+PRI_BG_COLOR = 'black'
+SEC_BG_COLOR = 'white'
+PRI_FT_COLOR = 'white'
+SEC_FT_COLOR = 'black'
+
+db = Database()
+
+class Landing(tk.Frame):
+    def __init__(self, copii, master):
+        super().__init__(master)
+        self.copii = copii
+        self.master = master
+        self.passcode_enter = tk.StringVar()
+        self.login_view()
+
+    def login(self):
+        pcode_frm_db = db.get_passcode(self.username)
+        if self.passcode_enter.get() == pcode_frm_db:
+            self.copii.navigation('insert')
+        self.master.unbind("<Return>")
+
+    def login_view(self):
+        if db.check_table_exists('credentials'):
+            # Exising user - show welcome message and passcode page
+            self.username = db.get_username()
+            welcome_msg = "Hello {}! Welcome back to Copii!".format(self.username)
+            tk.Label(self.master, text=welcome_msg, bg=PRI_BG_COLOR, fg=PRI_FT_COLOR).pack()
+            tk.Label(self.master, text='Passcode: ', bg=PRI_BG_COLOR, fg=PRI_FT_COLOR).pack()
+            tk.Entry(self.master, bg=SEC_BG_COLOR, fg=SEC_FT_COLOR, textvariable=self.passcode_enter, show='*').pack()
+            tk.Button(self.master, text='Submit', command=self.login).pack()
+            self.master.bind("<Return>", (lambda event: self.login()))
+
+
+class InsertRecord(tk.Frame):
+    def __init__(self, copii, master):
+        super().__init__(master)
+        self.copii = copii
+        self.master = master
+        self.insert()
+
+    def insert(self):
+        label = tk.Label(self.master, text='tis is insert')
+        label.pack()
+
+
 class Copii(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
@@ -10,16 +56,26 @@ class Copii(tk.Frame):
         # Width height
         self.master.geometry("600x700")
         # Set background color of copii
-        self.master.configure(background='black')
-        # Create widgets/grid
-        self.create_widgets()
+        self.master.configure(background=PRI_BG_COLOR)
+        self.navigation('landing')
 
-    def create_widgets(self):
-        label = tk.Label(self.master, text="testing")
-        label.pack()
+    def call_landing(self):
+        landing = Landing(self, self.master)
+
+    def call_insert(self):
+        insert = InsertRecord(self, self.master)
+
+    def navigation(self, to_page):
+        for widget in self.master.winfo_children():
+            widget.destroy()
+        if to_page == 'landing':
+            self.call_landing()
+        elif to_page == 'insert':
+            self.call_insert()
 
 
 if __name__ == '__main__':
+    # Start running app
     root = tk.Tk()
     copii = Copii(master=root)
     copii.mainloop()
--- database.py
+++ database.py
@@ -4,6 +4,22 @@ import sqlite3
 class Database:
     def __init__(self):
         self.con = self.get_connection()
+        self.login()
+
+    def check_table_exists(self, table):
+        query = "SELECT name FROM sqlite_master WHERE type='table' AND name='{}'".format(table)
+        if len(self.con.execute(query).fetchall()) == 0:
+            return False
+        else:
+            return True
+
+    def login(self):
+        table = 'credentials'
+        if not self.check_table_exists(table):
+            self.create_table(table)
+
+    # def create_account(self, username, passcode):
+    #     self.insert_data('credentials', username=username, passcode=passcode)
 
     def get_connection(self):
         conn = sqlite3.connect('data.db')
@@ -12,14 +28,16 @@ class Database:
     def close_connection(self):
         self.con.close()
 
-    def create_table(self):
-        query = "CREATE TABLE secrets (tag TEXT, secret TEXT)"
+    def create_table(self, table):
+        if table == 'secrets':
+            query = "CREATE TABLE secrets (tag TEXT, secret TEXT)"
+        elif table == 'credentials':
+            query = "CREATE TABLE credentials (username TEXT, passcode TEXT)"
         self.con.execute(query)
 
     def get_all_tags(self, table):
         # Create table if not exists
-        query = "SELECT name FROM sqlite_master WHERE type='table' AND name='{}'".format(table)
-        if len(self.con.execute(query).fetchall()) == 0:
+        if not self.check_table_exists('secrets'):
             self.create_table()
         get_query = "SELECT * FROM secrets"
         # return self.con.execute(get_query)
@@ -33,8 +51,23 @@ class Database:
         for val in res:
             print(val[0])
 
-    def insert_data(self, tag, secret):
-        query = "INSERT INTO secrets (tag, secret) VALUES ('{}', '{}')".format(tag, secret)
+    def get_username(self):
+        query = "SELECT username from credentials"
+        res = self.con.execute(query)
+        for val in res:
+            return val[0]
+
+    def get_passcode(self, username):
+        query = "SELECT passcode from credentials WHERE username = '{}'".format(username)
+        res = self.con.execute(query)
+        for val in res:
+            return val[0]
+
+    def insert_data(self, table, username=None, passcode=None, tag=None, secret=None):
+        if table == 'secrets':
+            query = "INSERT INTO '{}' (tag, secret) VALUES ('{}', '{}')".format(table, tag, secret)
+        elif table == 'credentials':
+            query = "INSERT INTO '{}' (username, passcode) VALUES ('{}', '{}')".format(table, username, passcode)
         self.con.execute(query)
         self.con.commit()
 
