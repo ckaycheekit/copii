b848621cabafb95572d2141aadb202138b30a9e0
ckaycheekit <ckayyeah@gmail.com>
2021-07-28 17:53:59 +0800

    Rough app framework

--- copii.py
+++ copii.py
@@ -1,10 +1,25 @@
-import tkinter
+import tkinter as tk
 from database import Database
 
 
+class Copii(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.master.title('copii')
+        # Width height
+        self.master.geometry("600x700")
+        # Set background color of copii
+        self.master.configure(background='black')
+        # Create widgets/grid
+        self.create_widgets()
+
+    def create_widgets(self):
+        label = tk.Label(self.master, text="testing")
+        label.pack()
+
+
 if __name__ == '__main__':
-    db = Database()
-    # db.get_all_tags('secrets')
-    db.delete_data('token')
-    # db.insert_data('token', 'i3i4bj3bj')
-    db.get_all_tags('secrets')
+    root = tk.Tk()
+    copii = Copii(master=root)
+    copii.mainloop()
